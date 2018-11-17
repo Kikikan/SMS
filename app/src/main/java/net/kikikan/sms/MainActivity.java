b7249a90182eaa7ff3618abeacfa8a91aa79be75
Kikikan <kikikkan@freemail.hu>
2018-11-17 17:08:54 +0100

    SMS küldés valszeg kész + UI

--- app/src/main/java/net/kikikan/sms/MainActivity.java
+++ app/src/main/java/net/kikikan/sms/MainActivity.java
@@ -1,13 +1,135 @@
 package net.kikikan.sms;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    final int SEND_SMS_PERMISSION_REQUEST_CODE = 1;
+    final String SENT = "SMS_SENT";
+    final String DELIVERED = "SMS_DELIVERED";
+    PendingIntent sentPI, deliveredPI;
+    BroadcastReceiver smsSentReceiver, smsDeliveredReceiver;
+
+    EditText number;
+    EditText message;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        number = findViewById(R.id.phoneNumberEditText);
+        message = findViewById(R.id.messageEditText);
+        button = findViewById(R.id.sendButton);
+
+        if (!hasPermission(Manifest.permission.SEND_SMS)) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS}, SEND_SMS_PERMISSION_REQUEST_CODE);
+        }
+
+        sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+        deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        smsSentReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                switch(getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getApplicationContext(), "SMS elküldve!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getApplicationContext(), "Generic failure!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getApplicationContext(), "Nincs térerő!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getApplicationContext(), "Null PDU!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getApplicationContext(), "Radio off!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+
+        smsDeliveredReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getApplicationContext(), "SMS kézbesítve!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getApplicationContext(), "SMS nem lett kézbesítve!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+
+        registerReceiver(smsSentReceiver, new IntentFilter(SENT));
+        registerReceiver(smsDeliveredReceiver, new IntentFilter(DELIVERED));
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(smsSentReceiver);
+        unregisterReceiver(smsDeliveredReceiver);
+    }
+
+    public boolean hasPermission(String perm) {
+        int id = ContextCompat.checkSelfPermission(this, perm);
+        return (id == PackageManager.PERMISSION_GRANTED);
+    }
+
+    public void onClick(View v) {
+        String phoneN = number.getText().toString();
+        String smsM = message.getText().toString();
+
+        if (phoneN == null || phoneN.length() == 0 || smsM == null || smsM.length() == 0)
+            return;
+
+        if (hasPermission(Manifest.permission.SEND_SMS)) {
+            SmsManager sm = SmsManager.getDefault();
+
+            sm.sendTextMessage(phoneN, null, smsM, sentPI, deliveredPI);
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS}, SEND_SMS_PERMISSION_REQUEST_CODE);
+            Toast.makeText(getApplicationContext(), "Permission megtagadva!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
